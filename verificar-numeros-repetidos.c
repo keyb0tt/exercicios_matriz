29875d1415884cbad637726ef628e22266a1bf5f
Kaique <kaiqueboscoaraujo00@gmail.com>
2025-10-10 10:16:10 -0300

    .

--- verificar-numeros-repetidos.c
+++ verificar-numeros-repetidos.c
@@ -31,14 +31,22 @@ int main(){
     // }
 
     
-    int vetorSec[9];
-    int vetor[9] = {1, 1, 1, 3, 4, 5, 6, 7, 8};
-    
-    bool find = false;
     int vetLenght = 0, vetLenght2 = 0;
+    int vetorSec[9];
     int rep[9];
+    int vetor[9] = {1, 1, 1, 3, 4, 2, 2, 2, 8};
+    bool find = false;
+
     
-    printf("\n\nFind/x\n");
+    printf("\nConteúdo do vetor\n\n");
+    for(x = 0; x <= 8; x++){
+        printf("%d ", vetor[x]);
+    }
+    
+    printf("\n ________________________ \n");
+    printf("|                        |");
+    printf("\n|   Find-----x-----rep   |\n");
+    printf("|                        |\n"); 
     for(x = 0; x <= 8; x++){
         for(y = 0; y <= 8; y++){
             if(vetor[x] == vetorSec[y]){
@@ -51,29 +59,41 @@ int main(){
         }
         if(find == false){
             vetorSec[vetLenght++] = vetor[x];
-            printf("Vetor[x] = %d\n", vetor[x]); 
+            printf("|      Vetor[%d] = %d      |\n", x, vetor[x]); 
         } else {
             vetorSec[vetLenght++] = 0;
             vetLenght2++;
         }
         find = false;
     }
+    printf("|                        |");
+    printf("\n|________________________|");
 
-    printf("\n\nVetorSec (0 = Null)\n");
+    printf("\n ________________________\n");
+    printf("|                        |\n");
+    printf("|   VetorSec(0 = Null)   |\n|   ");
     for(i = 0; i <= 8; i++){
         printf("%d ", vetorSec[i]);
     }
+    printf("   |\n|________________________|");
 
-    printf("\n\nRep (0 = Null)\n");
+
+    printf("\n ________________________\n");
+    printf("|                        |\n");
+    printf("|     Rep(0 = Null)      |\n|  ");
     for(i = 0; i <= 8; i++){
         printf("%d ", rep[i]);
     }
+    printf("  |\n|________________________|");
 
 
-    // printf("\nConteúdo do vetor\n");
-    // for(x = 0; x <= 8; x++){
-    //     printf("%d ", vetor[x]);
-    // }
+    printf("\n ________________________\n");
+    printf("|                        |\n");
+    printf("|     Vetlenght = %d      |\n", vetLenght);
+    printf("|     Vetlenght2 = %d     |\n", vetLenght2);
+    printf("|________________________|");
+
+
 
     return 0;
 }
